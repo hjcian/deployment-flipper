1ad0a8cf455c70b80e15f4eb1a3c74ebb6c3d8ea
Max Cian <maxcian@jubo.health>
2022-07-21 23:34:30 +0800

    feat(wip): wip

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ GO_CMD=go
 
 
 rundev:
-	$(GO_CMD) run cmd/localdev/main.go
-
+	air -c cmd/localdev/.air.toml
+# $(GO_CMD) run cmd/localdev/main.go
 tidy:
 	go mod tidy
--- core/core.go
+++ core/core.go
@@ -3,6 +3,7 @@ package core
 import (
 	"context"
 	"fmt"
+	"time"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
@@ -14,24 +15,57 @@ func LogClusterError(msg string, err error) {
 	}
 }
 
+type Controller struct {
+	clientset *kubernetes.Clientset
+}
+
+func (c *Controller) GetDeploymentLatestUpdatedTime(ns string) {
+	deployments, err := c.clientset.AppsV1().Deployments(ns).List(context.Background(), metav1.ListOptions{})
+	LogClusterError("get deployment in "+ns, err)
+	for _, deploy := range deployments.Items {
+		if !metav1.HasAnnotation(deploy.ObjectMeta, "deployment-flipper.watch") {
+			continue
+		}
+
+		conditions := deploy.Status.Conditions
+		for _, cond := range conditions {
+			fmt.Println(cond)
+		}
+	}
+}
+
 func Main(clientset *kubernetes.Clientset) {
+	controller := Controller{clientset}
 	for {
 		// Core logic:
 		// 1. Get all namespace names
 		// 	1.1 filter out namespaces with given include/exclude options
+
+		// namespaces, err := clientset.CoreV1().Namespaces().List(context.Background(), metav1.ListOptions{})
+		// LogClusterError("get namespaces", err)
+		// fmt.Println(len(namespaces.Items))
+
 		// 2. Get all Deployment in that namespaces
+		// for _, ns := range namespaces.Items {
+		// 	deployments, err := clientset.AppsV1().Deployments(ns.Name).List(context.Background(), metav1.ListOptions{})
+		// 	LogClusterError("get deployment in "+ns.Name, err)
+
+		// 	fmt.Println(ns.Name, len(deployments.Items))
+		// 	for _, deploy := range deployments.Items {
+		// 		fmt.Println(deploy.Namespace, deploy.Name)
+		// 		conditions := deploy.Status.Conditions
+		// 		for _, cond := range conditions {
+		// 			fmt.Println(cond)
+		// 		}
+		// 	}
+		// }
+
+		controller.GetDeploymentLatestUpdatedTime("default")
+
 		// 3. Get all referenced Secrets/ConfigMaps in each Deployment
 		// 4. Compare updated time on Deployment and Secrets/ConfigMaps
 		// 5. Restart the Deployment if its updated time is older than its referenced Secrets/ConfigMaps
 
-		namespaces, err := clientset.CoreV1().Namespaces().List(context.TODO(), metav1.ListOptions{})
-		LogClusterError("get namespaces", err)
-
-		fmt.Println(len(namespaces.Items))
-
-		for _, ns := range namespaces.Items {
-			fmt.Println(ns.Name)
-		}
 		// pods, err := clientset.CoreV1().Pods("").List(context.TODO(), metav1.ListOptions{})
 		// if err != nil {
 		// 	panic(err.Error())
@@ -55,6 +89,7 @@ func Main(clientset *kubernetes.Clientset) {
 		// 	fmt.Printf("Found pod %s in namespace %s\n", pod, namespace)
 		// }
 
-		break
+		fmt.Println("---------------------- [cluster] sleep 3s ----------------------")
+		time.Sleep(time.Second * 3)
 	}
 }
